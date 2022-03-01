e1692a16c67af596c0614a026f7ed55ccf80c4e6
kharelutsav <kharelutsav4@gmail.com>
2022-03-01 21:37:37 +0545

    Fetch Request for Library Performed Succesfully

--- src/REST_Controller/UI_Receiver.py
+++ src/REST_Controller/UI_Receiver.py
@@ -1,3 +1,5 @@
+from distutils.log import error
+from os import path, getcwd
 from REST_Controller.main import sio, app
 from flask import Response
 import socketio
@@ -5,11 +7,17 @@ from Middlewares.Middlewares import *
 
 
 @app.route("/", methods=["GET"])
-def getLibrary(request):
-    return Response("Hello, World!", 200, mimetype = "text/html")
+def getLibrary():
+    try:
+        with open("src\Data.json", "r") as data:
+            data = data.readlines()
+        return Response(data, 200, mimetype = "application/json")
+    except FileNotFoundError:
+        return Response("Test Failed!", 200, mimetype = "text/html")
+
 
 @app.route("/", methods=["POST",])
-def receiveUpdatesFrom_UI_Receiver(request):
+def receiveUpdatesFrom_UI_Receiver():
     return Response("Update Completed!", 200, mimetype = "text/html")
 
 
