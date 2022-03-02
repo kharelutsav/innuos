f29d0aeebc702bed237aea0af0c7deb519d732f8
kharelutsav <kharelutsav4@gmail.com>
2022-03-02 22:36:28 +0545

    Music without library shall be put into Unclassified Library

--- src/Database/Database_Interaction.py
+++ src/Database/Database_Interaction.py
@@ -3,21 +3,6 @@ from Database.DatabaseConnection import con
 
 cur = con.cursor()
 
-def getOnlyLibrariesFromDatabase():
-    data = []
-    query = "SELECT library_name FROM Library"
-    cur.execute(query)
-    for (lib,) in cur:
-        data.append(lib)
-    return data
-
-def getMusicsFromDatabase(library_name):
-    query = ("SELECT music_name FROM collection WHERE library_name = %s")
-    params = library_name
-    cur.execute(query, params)
-    collections = cur.fetchall()
-    return collections    
-
 def getLibrariesFromDatabase():
     buffer = {}
     query = "SELECT library.library_name, music.music_name, music.music_file, music.thumbnail_image\
@@ -28,7 +13,24 @@ def getLibrariesFromDatabase():
     cur.execute(query)
     for (library_name, music_name, file_path, image_path) in cur:
         if not library_name in buffer:
+            if library_name == None:
+                library_name = "Unclassified"
             buffer[library_name] = [[music_name, file_path, image_path]]
             continue
         buffer[library_name].append([music_name, file_path, image_path])
     return buffer
+
+# def getOnlyLibrariesFromDatabase():
+#     data = []
+#     query = "SELECT library_name FROM Library"
+#     cur.execute(query)
+#     for (lib,) in cur:
+#         data.append(lib)
+#     return data
+
+# def getMusicsFromDatabase(library_name):
+#     query = ("SELECT music_name FROM collection WHERE library_name = %s")
+#     params = library_name
+#     cur.execute(query, params)
+#     collections = cur.fetchall()
+#     return collections  
