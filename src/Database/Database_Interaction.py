e0a5a4dd4a9c59cf4e0c1cd9c96961b175881354
kharelutsav <kharelutsav4@gmail.com>
2022-03-04 12:18:50 +0545

    Bug: Playlists without music does not display

--- src/Database/Database_Interaction.py
+++ src/Database/Database_Interaction.py
@@ -6,33 +6,39 @@ cur = con.cursor()
 def getLibrariesFromDatabase():
     library = {}
     playlists = []
-    query = "SELECT library.library_name, music.music_name, music.music_file, music.thumbnail_image\
+    query = ("SELECT library.library_name, music.music_name, music.music_file, music.thumbnail_image\
         FROM library LEFT JOIN collection on\
         library.library_name = collection.library_name\
         RIGHT JOIN music on\
-        collection.music_name = music.music_name;"
+        collection.music_name = music.music_name;")
     cur.execute(query)
     for (library_name, music_name, file_path, image_path) in cur:
-        if not library_name in library:
-            if library_name == None:
-                library_name = "Unclassified"
+        if not library_name in playlists:
             playlists.append(library_name)
             library[library_name] = [[music_name, file_path, image_path]]
             continue
         library[library_name].append([music_name, file_path, image_path])
+    if None in playlists:
+        playlists.remove(None)
+        playlists.append("Unclassified")
+        library["Unclassified"] = library.pop(None)
     return playlists, library
 
-# def getOnlyLibrariesFromDatabase():
-#     data = []
-#     query = "SELECT library_name FROM Library"
-#     cur.execute(query)
-#     for (lib,) in cur:
-#         data.append(lib)
-#     return data
 
-# def getMusicsFromDatabase(library_name):
-#     query = ("SELECT music_name FROM collection WHERE library_name = %s")
-#     params = library_name
-#     cur.execute(query, params)
-#     collections = cur.fetchall()
-#     return collections  
+def addPlaylist(playlist):
+    query = ("INSERT INTO Library(library_name) VALUES (%s)")
+    values = [playlist,]
+    cur.execute(query, values)
+    con.commit()
+
+
+def addMusic(music_name, file_path, image_path):
+    query = ("INSERT INTO Music(music_name, music_file, thumbnail_image) VALUES (%s, %s, %s)")
+    values = (music_name, file_path, image_path)
+    cur.execute(query, values)
+    con.commit()
+
+
+def addMusicToPlaylist(playlist, ):
+    pass
+
--- src/REST_Controller/UI_Receiver.py
+++ src/REST_Controller/UI_Receiver.py
@@ -7,6 +7,8 @@ from src.Store.Cache import CACHE_STORE
 from decouple import config
 from os import path
 from Middlewares.Middlewares import lock
+from Database.Database_Interaction import addPlaylist, addMusic
+
 
 
 if CACHE_STORE.getCacheCount() == 0:
@@ -23,7 +25,7 @@ def getLibrary():
         return Response("Resource Not Found", 404, mimetype = "text/html")
 
 
-@app.route("/<playlist>", methods=["GET"]) ############### COMPLETED FOR NOW
+@app.route("/library/<playlist>", methods=["GET"]) ############### COMPLETED FOR NOW
 def getPlaylist(playlist):
     try:
         data = CACHE_STORE.getPlaylist(playlist)
@@ -32,7 +34,7 @@ def getPlaylist(playlist):
         return Response("Playlist Unavailable", 404, mimetype = "text/html")
 
 
-@app.route("/<playlist>/<song>", methods=["GET"]) ############### COMPLETED FOR NOW
+@app.route("/library/<playlist>/<song>", methods=["GET"]) ############### COMPLETED FOR NOW
 def streamMusic(playlist, song): 
     try:
         def generator():
@@ -42,31 +44,53 @@ def streamMusic(playlist, song):
                     yield music
                     music = read_music.read(1024)
         return Response(generator(), mimetype="audio/mp3")
-    except NotImplemented:
+    except:
         return Response("Playlist Unavailable", 404, mimetype = "text/html")
 
 
-@app.route("/upload/<Songname>", methods=["POST"])
+@app.route("/upload/<Songname>", methods=["POST"]) ########## FUNCTION TO HANDLE UPLOAD $$$$$$ VALIDATION $$$$$$
 def saveMusicFile(Songname):
-    print(request)
     music = request.files["file"]
     music.save(path.join(config("UPLOAD_FOLDER"),Songname))
     return Response(status=200)
 
-
-@app.route("/update", methods=["POST"])
-def receiveUpdatesFrom_UI_Receiver():
+@app.route("/addplaylist/<playlist>", methods=["POST"]) ######### PLAYLIST ADD METHOD IMPLEMENTED $$$$$ OPTIMIZE $$$$$
+def addPlaylistToLibrary(playlist):
     try:
-        with open("src\Store\Database.json", "r+") as read_json_file:
-            old_library = json.load(read_json_file)
-            old_library.update(request.json)
-            read_json_file.seek(0)
-            json.dump(old_library, read_json_file, indent=4)
-        return jsonify(old_library)
-    except TypeError:
-        return Response("Unable to submit form data.", 403, mimetype = "text/html")
-    except RuntimeError:
-        return Response("Unable to submit form data.", 403, mimetype = "text/html")
+        addPlaylist(playlist)
+        updateCache()
+        return Response(f"{playlist} added to the library.", 200, mimetype="text/html")
+    except:
+        return Response("Playlist already exists in the Library.", 403, mimetype = "text/html")
+
+@app.route("/addmusic/<music>", methods=["POST"]) ######### MUSIC ADD METHOD IMPLEMENTED $$$$$ OPTIMIZE $$$$$
+def addMusicToLibrary(music):
+    try:
+        addMusic(music, "test/path/2", "default/path")
+        updateCache()
+        return Response(f"{music} added to the library.", 200, mimetype="text/html")
+    except:
+        return Response("Music already exists in the Library.", 403, mimetype = "text/html")
+
+
+# @app.route("/update", methods=["POST"])
+# def receiveUpdatesFrom_UI_Receiver():
+#     try:
+#         with open("src\Store\Database.json", "r+") as read_json_file:
+#             old_library = json.load(read_json_file)
+#             old_library.update(request.json)
+#             read_json_file.seek(0)
+#             json.dump(old_library, read_json_file, indent=4)
+#         return jsonify(old_library)
+#     except TypeError:
+#         return Response("Unable to submit form data.", 403, mimetype = "text/html")
+#     except RuntimeError:
+#         return Response("Unable to submit form data.", 403, mimetype = "text/html")
+
+
+@app.errorhandler(404)
+def pageNotFound(e):
+    return Response("The path you requested does not exist.", 404, mimetype = "text/html")
 
 
 application = socketio.WSGIApp(sio, app)
